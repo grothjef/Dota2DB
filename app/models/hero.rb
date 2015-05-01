08a4d73dbd494b3485c977386e3bb796e24c0dd1
Jeff Groth <grothjef@gmail.com>
2015-05-01 18:04:14 -0400

    turnrate now numaricality validation

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -4,7 +4,7 @@ class Hero < ActiveRecord::Base
 
 	validates_format_of :movespeed, with: /\A\d{3}\z/, message: "isn't a correctly formatted move speed: e.g. 420", allow_blank: true
 
-	validates_format_of :turnrate, with: /\A\d{1}.\d{1,2}\z/, message: "isn't a correctly formatted turn rate: e.g. 0.4", allow_blank: true
+	validates :turnrate, numericality: { greater_than_or_equal_to: 0.0, less_than_or_equal_to: 1.0}
 
 	validates_format_of :sightrange, with: /\A\d{4}\/\d{3,4}\z/, message: "isn't a correctly formatted sight range: e.g. 1800/800", allow_blank: true
 
